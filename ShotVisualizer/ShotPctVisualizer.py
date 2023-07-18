Fixed coordinates for ShotVisulaizer Rink;Changeed getPlayShotPCT in PlayerQuery to Query from Boxscores(less computation)

ShotVisualizer/ShotPctVisualizer.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as img
+import numpy as np
 class ShotPctVisualizer:
     def __init__(self, shotTuple: tuple[int, int, float]) -> None:
         
@@ -7,9 +8,26 @@ class ShotPctVisualizer:
         self.totalGoals = shotTuple[1]
         self.shotPCT = shotTuple[2]
         self.colour = ["#ADD8E6","#90EE90"]
-        self.label = ["Total Shot on Net", "Total Goal"]
+        self.labels = ["Non-Converted Shots", "Converted Shots"]
 
     def piePlot(self):
         fig, ax = plt.subplots(figsize = (10,10))
-        ax.pie(x=[self.totalShots - self.totalGoals, self.totalGoals], colors=self.colour, autopct='%.0f%%', labels=self.label)
+        labels = [" ".join([self.labels[0],"{:.2%}".format(1-self.shotPCT)]), " ".join([self.labels[1],"{:.2%}".format(self.shotPCT)])]
+        ax.pie(x=[self.totalShots - self.totalGoals, self.totalGoals], colors=self.colour, wedgeprops=dict(width=0.5),startangle=-40)
+        
+        ax.legend(labels=labels)
+        #from https://matplotlib.org/stable/gallery/pie_and_polar_charts/pie_and_donut_labels.html
+        # bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
+        # kw = dict(arrowprops=dict(arrowstyle="-"),
+        #         bbox=bbox_props, zorder=0, va="center")
+
+        # for i, p in enumerate(wedges):
+        #     ang = (p.theta2 - p.theta1)/2. + p.theta1
+        #     y = np.sin(np.deg2rad(ang))
+        #     x = np.cos(np.deg2rad(ang))
+        #     horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+        #     connectionstyle = f"angle,angleA=0,angleB={ang}"
+        #     kw["arrowprops"].update({"connectionstyle": connectionstyle})
+        #     ax.annotate(self.label[i], xy=(x, y), xytext=(1.35*np.sign(x), 1.4*y),
+        #                 horizontalalignment=horizontalalignment, **kw)
         return fig

ShotVisualizer/ShotVisualizer.py
@@ -40,19 +40,18 @@ class ShotVisualizer:
 
         fig, ax = plt.subplots(figsize = (10,10))
         ax.axis('off')
-
-        
+        ax.set_xlim(-42.5,42.5)
+        ax.set_ylim(0, 100)
+        ax.set_xticks(np.arange(-42.5,42.5+1,0.1))
+        ax.set_yticks(np.arange(0, 100+1,0.1))
+    
         if graphType in ["heat","HEAT","Heat"]:
-            sns.kdeplot(x=coorYs, y=coorXs, fill=True, thresh=0.05,  alpha = 0.65)
+            sns.kdeplot(x=coorYs, y=coorXs, fill=True, thresh=0.05,  alpha = 0.65, clip=((-42.5,42.5),(0,100)))
             implot = ax.imshow(im, extent=(-42.5,42.5,0,100))    
 
         else:
             
             ax.scatter(x=coorYs, y=coorXs, c="r", s=60)
-            ax.xlim(-42.5,42.5)
-            ax.ylim(0, 100)
-            ax.xticks(np.arange(-42.5,42.5+1,0.1))
-            ax.yticks(np.arange(0, 100+1,0.1))
             implot = ax.imshow(im, extent=(-42.5,42.5,0,100))
 
         return fig
@@ -66,7 +65,7 @@ class ShotVisualizer:
                 print(event)
                 break
             if event["coorX"] < 0:
-                event["coorY"],event["coorX"] = -event["coorY"],-event["coorX"]
+                event["coorY"],event["coorX"] = event["coorY"],-event["coorX"]
             output.append(event)
 
         return output